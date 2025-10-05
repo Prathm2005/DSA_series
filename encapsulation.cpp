5faab69a362b3c262b3172f7998a89700dc01e0f
Prathmesh <prathmesh.malunjkar02@gmail.com>
2025-10-05 11:40:30 +0530

    encapsulation

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,36 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-class Customer{
+class Student{
     string name;
-    int balance;
     int age;
 
     public:
-    Customer(string a,int b,int c){
-        name=a;
-        balance=b;
-        age=c;
 
+    void setname(string name,int age){
+       this->name=name;
+       this->age=age;
     }
 
-    void deposit(int amount){
-        if(amount>0){
-            balance+=amount;
-        }
-        else{
-            cout<<"Invalid amount"<<endl;
-        }
-    }
-
-    void display(){
-        cout<<name<<" "<<balance<<" "<<age<<endl;
+    void getname(){
+        cout<<name<<" "<<age<<endl;
     }
 };
 
+
+
 int main(){
-    Customer c1("Prathmesh",0,21);
-    c1.deposit(100);
-    c1.display();
+    Student s1;
+    s1.setname("Rohit",45);
+    s1.getname();
 }
